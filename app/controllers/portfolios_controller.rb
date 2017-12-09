370fe887fe5c7f55367ea6078db0a61942806f48
roryjackd <roryjackd@yahoo.com>
2017-12-08 23:52:03 -0700

    Refactored portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
 
   def index
@@ -27,11 +28,11 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end  
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
+    
 
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -43,11 +44,11 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end  
 
   def destroy 
-    @portfolio_item = Portfolio.find(params[:id])
+    
     
     @portfolio_item.destroy
 
@@ -64,6 +65,10 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       technologies_attributes: [:name]
                                       )
+  end  
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end  
 
 end
