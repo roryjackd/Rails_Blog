ea98f3551589ed3b1045ac3f399f7a71307c6296
roryjackd <roryjackd@yahoo.com>
2017-12-24 18:43:39 -0700

    Form refactor finished, gritter added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -62,4 +62,17 @@ end
   def active? path
     "active" if current_page? path
   end  
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end  
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Rory Demers Portfolio", sticky: false)
+  end  
+
 end
